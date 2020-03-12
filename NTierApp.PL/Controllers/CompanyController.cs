8a7e9aea5fa5ec1cd80f7ff2bc0cb74b2aa19295
vmalash <vmalash@cryptogateways.io>
2020-03-12 13:18:34 +0300

    Add CompanyController and EmployeeController

---
+++ NTierApp.PL/Controllers/CompanyController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NTierApp.BLL.Models;
+
+namespace NTierApp.PL.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private MsSqlContext db = new MsSqlContext();
+
+        // GET: Company
+        public ActionResult Index()
+        {
+            return View(db.CompanyModels.ToList());
+        }
+
+        // GET: Company/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyModel companyModel = db.CompanyModels.Find(id);
+            if (companyModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyModel);
+        }
+
+        // GET: Company/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Company/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address")] CompanyModel companyModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CompanyModels.Add(companyModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(companyModel);
+        }
+
+        // GET: Company/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyModel companyModel = db.CompanyModels.Find(id);
+            if (companyModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyModel);
+        }
+
+        // POST: Company/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address")] CompanyModel companyModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(companyModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(companyModel);
+        }
+
+        // GET: Company/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyModel companyModel = db.CompanyModels.Find(id);
+            if (companyModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyModel);
+        }
+
+        // POST: Company/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CompanyModel companyModel = db.CompanyModels.Find(id);
+            db.CompanyModels.Remove(companyModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NTierApp.PL/MsSqlContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+public class MsSqlContext : DbContext
+{
+	public MsSqlContext() : base("name=DefaultConnection")
+	{
+	}
+
+	public DbSet<NTierApp.BLL.Models.EmployeeModel> EmployeeModels { get; set; }
+
+	public DbSet<NTierApp.BLL.Models.CompanyModel> CompanyModels { get; set; }
+}
